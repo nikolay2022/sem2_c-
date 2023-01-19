1e98f1b588ecb48cad00b9765454cd43104314f1
nikolay <94001931+nikolay2022@users.noreply.github.com>
2023-01-19 16:45:12 +0400

    Add files via upload

---
+++ lab2/README.ru.md
@@ -0,0 +1,13 @@
+# lab1
+[![en](https://img.shields.io/badge/lang-en-red.svg)](https://github.com/nikolay2022/sem2_csharp/blob/main/lab1/README.md)
+
+Составить программу для вычисления значения функции. 
+Предусмотреть ввод заданного количества параметров. Для проверки 
+правильности программы после задания приведен контрольный пример: 
+тестовые значения переменных, используемых в выражении, и результат, 
+который при этом получается.
+
+![equation](http://www.sciweavers.org/tex2img.php?eq=1%2Bsin%28mc%5E2%29&bc=White&fc=Black&im=jpg&fs=12&ff=arev&edit=)
+
+
+---
---
+++ lab2/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        double x, y, z;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Начальное значение X
+            textBox1.Text = "17,421";
+            // Начальное значение Y
+            textBox2.Text = "0,010365";
+            // Начальное значение Z
+            textBox3.Text = "82800";
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            try
+            {
+                x = Convert.ToDouble(textBox1.Text);
+            }
+            catch
+            { MessageBox.Show("Некорректное значение x. Попробуйте еще раз."); }
+
+            try
+            {
+
+                y = Convert.ToDouble(textBox2.Text);
+            }
+            catch { MessageBox.Show("Некорректное значение y. Попробуйте еще раз."); }
+         
+            try
+            {
+                z = Convert.ToDouble(textBox3.Text);
+            }
+            catch { MessageBox.Show("Некорректное значение z. Попробуйте еще раз."); }
+
+            double f = (Math.Pow(y + Math.Pow(x-1,1.0/3.0),1.0/4.0)) / (Math.Abs(x - y) * (Math.Pow(Math.Sin(z),2) + Math.Tan(z))) ;
+    
+           
+
+
+            if (!(textBox1.Text == "" || textBox2.Text == "" || textBox3.Text == "")) // проверка на то введены ли значения
+            {
+                textBox4.Text += Environment.NewLine + "Значение x = " + x.ToString();
+                textBox4.Text += Environment.NewLine + "Значение y = " + y.ToString();
+                textBox4.Text += Environment.NewLine + "Значение z = " + z.ToString();
+                textBox4.Text += Environment.NewLine + "Результат f = " + f.ToString();
+            }
+            else
+            {
+                MessageBox.Show("Поля не должны быть пустыми");
+            }
+            
+        }
+    }
+}
